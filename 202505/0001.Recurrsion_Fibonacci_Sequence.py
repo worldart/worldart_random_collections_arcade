8c75c03f98e070ab429fcd0ff92dd45e92eb3cd2
HIDDENUSER <darkresearchlab@gmail.com>
2025-05-21 21:56:12 +0100

    Update 0001.Recurrsion_Fibonacci_Sequence.py

--- 202505/0001.Recurrsion_Fibonacci_Sequence.py
+++ 202505/0001.Recurrsion_Fibonacci_Sequence.py
@@ -1,3 +1,7 @@
+#v1
+
+
+
 def fib(n):
     a = b = 1
 
@@ -18,3 +22,36 @@ def fib(n):
 
 # Example usage:
 #fib(10)
+
+
+
+
+
+
+#v2
+
+
+
+
+
+
+
+def main():
+    a, b = 1, 1
+
+    n = int(input("Enter how many numbers to print: "))
+
+    # Print the first two numbers
+    if n >= 1:
+        print(a)
+    if n >= 2:
+        print(b)
+
+    # Print the rest
+    for i in range(3, n + 1):
+        c = a + b
+        print(c)
+        a, b = b, c
+
+if __name__ == "__main__":
+    main()
